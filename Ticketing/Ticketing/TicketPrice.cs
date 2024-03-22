f52abc21e122f09b5a4836dbe1f0efe1535b3a83
Xiaorui Guo <xiaorui.guo@tafesa.edu.au>
2024-03-22 22:06:37 +1030

    commit the branch 1.1

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -14,7 +14,7 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 35.5m;
+        const decimal mdecBalcony = 35m;
         const decimal mdecGeneral = 28.75m;
         const decimal mdecBox = 63.0m;
         const decimal mdecDiscount = 5.0m; 
